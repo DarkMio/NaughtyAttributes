32dda8b4db11bab656a9faaaaa75013b953295e2
Martin Zier <martin.zier@island-labs.com>
2021-01-18 15:01:26 +0100

    Enabling basic unit tests

---
+++ Packages/com.dbrizov.naughtyattributes/Tests/Editor/EditModeVisibility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NaughtyAttributes.Editor;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace NaughtyAttributes.Tests
+{
+    public class EditModeVisibility
+    {
+        [Test]
+        public void IsVisibleInEditMode()
+        {
+            var methodInfo = ((Action) VisibilityClass.HiddenInPlayModeFunction).Method;
+            Assert.IsTrue(ButtonUtility.IsVisible(null, methodInfo),
+                "Method with no EditMode Visibility Modifier should be visible.");
+        }
+
+        [Test]
+        public void IsNotVisibleInEditMode()
+        {
+            var methodInfo = ((Action) VisibilityClass.HiddenInEditModeFunction).Method;
+            var isVisible = ButtonUtility.IsVisible(null, methodInfo);
+            Assert.IsFalse(isVisible,
+                "Method with [HideInEditMode] should not be visible in EditMode");
+        }
+
+
+
+        private class VisibilityClass : ScriptableObject
+        {
+            [HideInEditMode] public string hiddenInEditMode;
+            [HideInEditMode] public static void HiddenInEditModeFunction() { }
+            [HideInPlayMode] public static void HiddenInPlayModeFunction() { }
+            [HideInEditMode, HideInPlayMode] public static void HiddenInEditAndPlayMode() { }
+        }
+    }
+}
---
+++ Packages/com.dbrizov.naughtyattributes/Tests/Runtime/PlayModeVisibility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using NaughtyAttributes.Editor;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace NaughtyAttributes.Tests
+{
+    public class VisibilityTest
+    {
+        [Test]
+        public void IsVisibleInPlayer()
+        {
+            var methodInfo = ((Action) VisibilityClass.HiddenInEditModeFunction).Method;
+            Assert.IsTrue(ButtonUtility.IsVisible(null, methodInfo),
+                "Method with no PlayMode Visibility Modifier should be visible.");
+        }
+
+        [Test]
+        public void IsNotVisibleInPlayer()
+        {
+            var methodInfo = ((Action) VisibilityClass.HiddenInPlayModeFunction).Method;
+            var isVisible = ButtonUtility.IsVisible(null, methodInfo);
+            Assert.IsFalse(isVisible,
+                "Method with [HideInPlayMode] should not be visible in PlayMode.");
+        }
+
+
+
+        private class VisibilityClass : ScriptableObject
+        {
+            [HideInEditMode] public string hiddenInEditMode;
+            [HideInEditMode] public static void HiddenInEditModeFunction() { }
+            [HideInPlayMode] public static void HiddenInPlayModeFunction() { }
+            [HideInEditMode, HideInPlayMode] public static void HiddenInEditAndPlayMode() { }
+        }
+    }
+}
